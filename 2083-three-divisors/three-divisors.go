b9f79c0398228b66e1145e714786022f62d357db
pransh1 <124560561+pransh1@users.noreply.github.com>
2025-10-25 01:26:18 +0530

    Time: 0 ms (100.00%) | Memory: 4 MB (2.86%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.go
@@ -0,0 +1,17 @@
+func isThree(n int) bool {
+    if n <= 2 {
+        return false
+    }
+
+    count := 0
+    for i := 1; i <= n; i++ {
+        if n % i == 0 {
+            count++
+        }
+    }
+
+    if count == 3 {
+        return true
+    }
+    return false
+}
