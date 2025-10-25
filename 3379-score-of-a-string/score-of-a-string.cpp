38e8aeb30f7b9250b89f0849d82342ffaa256097
pransh1 <124560561+pransh1@users.noreply.github.com>
2025-10-25 23:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (26.39%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -5,7 +5,7 @@ public:
         int sum = 0;
         for(int i=0;i<n-1;i++) {
             sum += abs(s[i] - s[i+1]);
-            cout<<sum<<endl; 
+            // cout<<sum<<endl; 
         }
         return sum;
     }
