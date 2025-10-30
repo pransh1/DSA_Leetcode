eec9c837fe8bc122e5c5ef428daa3b228f06efab
pransh1 <124560561+pransh1@users.noreply.github.com>
2025-10-30 11:38:58 +0530

    Time: 0 ms (100.00%) | Memory: 102.1 MB (67.69%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     int islandPerimeter(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        vector<vector<int>> vis(n, (vector<int>(m, 0)));
-        queue<pair<int, int>> q;
+        int perimeter = 0;
 
         for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
+            for(int j =0;j<m;j++) {
                 if(grid[i][j] == 1) {
-                    q.push({i, j});
-                    vis[i][j] = 1;
-                    break;
-                }
-            }
-        }
+                    perimeter += 4;
 
-        if(q.empty()) return 0;
-
-        int perimeter = 0;
-        while(!q.empty()) {
-            auto [x, y] = q.front();
-            q.pop();
-            int side = 4;
-
-            for(auto& d : directions) {
-                int nx = x + d[0];
-                int ny = y + d[1];
-
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m && grid[nx][ny] == 1) {
-                    side -= 1;
-                    if(!vis[nx][ny]) {
-                        vis[nx][ny] = 1;
-                        q.push({nx, ny});
+                    if(i < n-1 && grid[i+1][j] == 1) {
+                        perimeter -= 2;
                     }
 
+                    if(j < m-1 && grid[i][j+1] == 1) {
+                        perimeter -= 2;
+                    }
                 }
             }
-            perimeter += side;
         }
         return perimeter;
     }
