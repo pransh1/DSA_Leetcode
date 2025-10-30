34054679f81ce189efaa09da9aa6e6dcdfdefa20
pransh1 <124560561+pransh1@users.noreply.github.com>
2025-10-30 12:54:37 +0530

    Time: 1360 ms (5.06%) | Memory: 215.7 MB (5.31%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int minimumDeleteSum(string s1, string s2) {
-        int n = s1.size(), m = s2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s1[i-1] == s2[j-1])
-                    dp[i][j] = dp[i-1][j-1] + (int)s1[i-1];
-                else
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+    unordered_map<string, int> mp;
+    int solve(int i, int j, string& s1, string& s2) {
+        if(i == s1.size () && j == s2.size()) return 0;
+        // if s2 still has some val
+        if(i == s1.size()) {
+            int sum = 0;
+            for(int k = j; k<s2.size(); k++) {
+                sum += (int)s2[k];
             }
+            return sum;
+        } 
+        // if s1 still has elements
+        if(j==s2.size()) {
+            int sum = 0;
+            for(int k=i;k<s1.size(); k++) {
+                sum += (int)s1[k];
+            }
+            return sum;
         }
 
-        // Total ASCII of both strings
-        int total1 = 0, total2 = 0;
-        for (char c : s1) total1 += (int)c;
-        for (char c : s2) total2 += (int)c;
+        // create unique key for memoization
+        string key = to_string(i) + "," + to_string(j);
+        if (mp.count(key)) return mp[key];
+        // if both same skip
+        if(s1[i] == s2[j]) return solve(i+1, j+1, s1, s2);
         
-        return total1 + total2 - 2 * dp[n][m];
+        // either del from s1 first or s2
+        int delS1 = (int)s1[i] + solve(i+1, j, s1, s2);
+        int delS2 = (int)s2[j] + solve(i, j+1, s1, s2);
+        return mp[key] = min(delS1, delS2);        
+    }
+    int minimumDeleteSum(string s1, string s2) {
+        return solve(0, 0, s1, s2);
     }
 };
