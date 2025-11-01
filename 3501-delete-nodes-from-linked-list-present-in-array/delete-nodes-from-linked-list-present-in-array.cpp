a8ae66065a3e1a393d91c1baefd4d47247bd9307
pransh1 <124560561+pransh1@users.noreply.github.com>
2025-11-01 20:30:59 +0530

    Time: 84 ms (91.72%) | Memory: 263.4 MB (58.72%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -8,21 +8,36 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_set<int> s(nums.begin(), nums.end()); // push nums into set
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode* curr = &dummy;
-        while(curr->next!=NULL){
-            if(s.count(curr->next->val)){
-                curr->next = curr->next->next;
-            }
-            else {
-                curr = curr->next;
+        unordered_set<int> s(nums.begin(), nums.end());
+        
+        // remove nodes from start if they are in nums
+        while (head && s.count(head->val)) {
+            head = head->next;
+        }
+
+        // now traverse and remove internal nodes
+        ListNode* temp = head;
+        while (temp && temp->next) {
+            if (s.count(temp->next->val)) {
+                temp->next = temp->next->next;
+            } else {
+                temp = temp->next;
             }
         }
-        return dummy.next;
+
+        return head;
     }
 };
