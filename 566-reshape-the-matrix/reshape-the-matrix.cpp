0370fc04c3797f6558bf747144cb8370538e940b
pransh1 <124560561+pransh1@users.noreply.github.com>
2025-11-14 10:31:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (76.82%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        int n = mat.size();
+        int m = mat[0].size();
+        if( n* m != r*c) {
+            return mat;
+        }      
+        else {
+            vector<vector<int>>new_mat(r, vector<int>(c, 0));
+            int row = 0;
+            int col = 0;
+                    
+            for(int i=0;i<n;i++) {
+                for(int j=0;j<m;j++){
+                    new_mat[row][col] = mat[i][j];
+                    col++;
+                    if(col == c) {
+                        col = 0;
+                        row++;
+                    }
+                }
+            }
+            return new_mat;
+        }
+        return mat;
+    }
+};
