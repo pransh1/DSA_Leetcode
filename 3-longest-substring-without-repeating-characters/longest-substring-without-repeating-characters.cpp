557331dae708383b6d2c41d039e4521a8f8eeb94
pransh1 <124560561+pransh1@users.noreply.github.com>
2025-11-17 10:32:49 +0530

    Time: 19 ms (29.57%) | Memory: 14.3 MB (29.79%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,43 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n = s.size();
+//         int maxLen = 0;
+
+//         for(int l=0;l<n;l++) {
+//             unordered_map<char, int> mp;
+//             for(int r=l;r<n;r++) {
+//                 char ch = s[r];
+//                 if(mp[ch] > 0) break;
+//                 mp[ch]++;
+//                 int len = r-l+1;
+//                 maxLen = max(len, maxLen);
+//             }
+//         }
+//         return maxLen;
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        int maxLen = 0;
+        unordered_set<char> st;
+        
+        int l = 0;
+        int ans = 0;
 
-        for(int l=0;l<n;l++) {
-            unordered_map<char, int> mp;
-            for(int r=l;r<n;r++) {
-                char ch = s[r];
-                if(mp[ch] > 0) break;
-                mp[ch]++;
-                int len = r-l+1;
-                maxLen = max(len, maxLen);
+        for (int r = 0; r < n; r++) {
+            // if character already in window, shrink from left
+            while (st.count(s[r])) {
+                st.erase(s[l]);
+                l++;
             }
+
+            st.insert(s[r]);
+            ans = max(ans, r - l + 1);
         }
-        return maxLen;
+
+        return ans;
     }
 };
