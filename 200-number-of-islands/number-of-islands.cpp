2b9c1d85009ccd5e0194d56b6dfe5c567e77f8ec
pransh1 <124560561+pransh1@users.noreply.github.com>
2025-12-02 12:44:05 +0530

    Time: 28 ms (50.18%) | Memory: 18.4 MB (40.66%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,75 +1,35 @@
-class DisJoint {
-    public:
-    vector<int> parent;
-    vector<int> size;
-
-    DisJoint(int n){
-        parent.resize(n);
-        size.resize(n, 1);
-
-        for(int i=0;i<n;i++){
-            parent[i] = i;
-        }
-    }
-
-    int findUltimateParent(int node){
-        if(parent[node] == node) return node;
-
-        return parent[node] = findUltimateParent(parent[node]);
-    }
-
-    void unionBySize(int u, int v) {
-        int parentU = findUltimateParent(u);
-        int parentV = findUltimateParent(v);
-
-        if (parentU == parentV) return;
-
-        if(size[parentU] < size[parentV]){
-            size[parentV] += size[parentU];
-            parent[parentU] = parentV;
-        }
-        else {
-            size[parentU] += size[parentV];
-            parent[parentV] = parentU;
-        }
-    }
-};
-
-
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        DisJoint ds(n * m);
-        int totalLand = 0;
+        int count = 0;
+        queue<pair<int, int>> q;
+        vector<vector<int>> dirs = {{-1,0}, {0,-1}, {1,0}, {0,1}};
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(grid[i][j] == '1') {
+                    count++;
+                    q.push({i, j});
+                    grid[i][j] = '0';
+                    while(!q.empty()) {
+                        auto [x, y] = q.front();
+                        q.pop();
 
-        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] =='1') {
-                    totalLand++;
-                    int node = i * m + j;
-                    for(auto &dir : directions){
-                        int ni = i + dir.first;
-                        int nj = j + dir.second;
+                        for(auto& dir : dirs) {
+                            int nx = x + dir[0];
+                            int my = y + dir[1];
 
-                        if (ni >= 0 && ni < n && nj >= 0 && nj < m && grid[ni][nj] == '1') {
-                            int adjNode = ni * m + nj;
-
-                            int parent1 = ds.findUltimateParent(node);
-                            int parent2 = ds.findUltimateParent(adjNode);
-
-                            if(parent1 != parent2) {
-                                ds.unionBySize(node, adjNode);
-                                totalLand--;
+                            if(nx >= 0 && nx < n && my >=0 && my < m && grid[nx][my] == '1') {
+                                grid[nx][my] = '0';
+                                q.push({nx,my});
                             }
                         }
                     }
                 }
             }
         }
-        return totalLand;
+        return count;
     }
 };
