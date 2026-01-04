3fb37986e4cc67233fc27b16a19fb1afdb5c659c
pransh1 <124560561+pransh1@users.noreply.github.com>
2026-01-04 12:53:09 +0530

    Time: 35 ms (42.86%) | Memory: 16.7 MB (28.82%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    int divisor_count(int n) {
+        int count = 0;
+        for(int i=1;i*i<=n;i++) {
+            if(n%i==0) {
+                if(i*i==n) {
+                    count+=1;
+                }
+                else {
+                    count +=2;
+                }
+            }
+        }
+        return count;
+    }
+
+    vector<int> divisor_pair_vec(int n, vector<int>& pair) {
+        pair.clear();
+        for(int i=1; i*i<=n;i++) {
+            if(n%i == 0) {
+                pair.push_back(i);
+
+                if(i*i != n) {
+                    pair.push_back(n/i);
+                }
+            }
+        }
+        return pair;
+    }
+
+    int sumFourDivisors(vector<int>& nums) {
+        int sum_of_divisor = 0;
+        vector<int> divisors;
+
+        for(int i=0;i<nums.size(); i++) {
+            if(divisor_count(nums[i]) == 4) {
+                divisor_pair_vec(nums[i], divisors);
+                for(int d: divisors) {
+                    sum_of_divisor += d;
+                }
+            }
+        }
+
+        return sum_of_divisor;
+    }
+};
