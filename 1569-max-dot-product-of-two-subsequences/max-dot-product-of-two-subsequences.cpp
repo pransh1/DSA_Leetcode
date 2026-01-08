0c817b3acb387972836b56982be5197d1473fd2f
pransh1 <124560561+pransh1@users.noreply.github.com>
2026-01-08 23:47:26 +0530

    Time: 10 ms (89.20%) | Memory: 17.7 MB (65.20%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -30,11 +30,23 @@ public:
         return memo[i][j] = max({take, skip1, skip2});
     }
 
-    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        n = nums1.size();
-        m = nums2.size();
-        memo.assign(n, vector<int>(m, INT_MIN));
+    int maxDotProduct(vector<int>& A, vector<int>& B) {
+        n = A.size();
+        m = B.size();
+        // memo.assign(n, vector<int>(m, INT_MIN));
 
-        return solve(0, 0, nums1, nums2);
+        // return solve(0, 0, nums1, nums2);
+
+        int n = A.size(), m = B.size();
+        vector<vector<int>> dp(n, vector<int>(m));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                dp[i][j] = A[i] * B[j];
+                if (i && j) dp[i][j] += max(dp[i - 1][j - 1], 0);
+                if (i) dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+                if (j) dp[i][j] = max(dp[i][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n - 1][m - 1];
     }
 };
